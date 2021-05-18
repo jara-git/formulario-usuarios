f8b9693a3f2672265f7e5d3f54dd0db65333dd72
jara <jarabelmonte@gmail.com>
2021-05-18 20:46:02 +0200

    first commit

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  name: string,
+  surname: string,
+  age: number,
+  dni: string,
+  bday: Date,
+  favColor: string,
+  sex : string
+}
+
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,73 @@
+import { isGeneratedFile } from '@angular/compiler/src/aot/util';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+
+export class UsersComponent implements OnInit {
+  position: number = 1;
+  action: string = 'insert;'
+
+  usersList: Array<User> = [];
+
+  currentUser: User = {
+    name: '',
+    surname: '',
+    age: 0,
+    dni: '',
+    bday: new Date(),
+    favColor : '',
+    sex : ''
+  };
+
+  registerUser( form=NgForm ):void {
+    if (this.action === 'insert') {
+     
+        this.usersList.push(this.currentUser)
+        this.currentUser = {
+          name: "",
+          surname: "",
+          age: 0,
+          dni: "",
+          bday: new Date(),
+          favColor : "",
+          sex : ""
+        
+    }
+    } else {
+      this.usersList[this.position] = this.currentUser;
+      this.action = 'insert';
+    }
+    this.currentUser ={
+        name: "",
+        surname: "",
+        age: 0,
+        dni: "",
+        bday: new Date(),
+        favColor : "",
+        sex : ""
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  deleteUser(indexPosition: number) : void {
+    this.usersList.splice(indexPosition,1)
+
+  }
+
+  updateUser(indexPosition: number): void {
+    this.currentUser = this.usersList[indexPosition];
+    this.action = "update";
+    this.position = indexPosition;
+  }
+
+}
